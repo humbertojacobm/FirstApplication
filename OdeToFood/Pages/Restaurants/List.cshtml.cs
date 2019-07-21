dc71f36c29aa20b9ab44f6e7041aa1eeaceb614d
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-07-20 23:13:51 -0500

    module: drilling into data, registering a data services

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,18 +5,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
 
         public string Message { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             _config = config;
+            _restaurantData = restaurantData;
         }
 
         public void OnGet()

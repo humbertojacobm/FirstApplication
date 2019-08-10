02f7007806bdce66ac0bb636220c7cfa08809aeb
Humberto Jacob Mori Guerra <hmori@belatrix.com>
2019-08-10 18:53:11 -0500

    module: model binding -> linking to the detail page

--- OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -11,9 +11,11 @@ namespace OdeToFood.Pages.Restaurants
     public class DetailModel : PageModel
     {
         public Restaurant Restaurant { get; set; }
-        public void OnGet()
+        public void OnGet(int restaurantId)
         {
             Restaurant = new Restaurant();
+            Restaurant.Id = restaurantId;
+
         }
     }
 }

2f7f91069fdba90fc987453974153e38e5e3bccf
humbertojacobm <humbertojacobm@gmail.com>
2019-08-22 22:28:34 -0500

    module: editing data -> confirming last operation

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -62,6 +62,7 @@ namespace OdeToFood.Pages.Restaurants
             }
             
             restaurantData.Commit();
+            TempData["Message"] = "Restaurant saved!";
             return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
 
         }

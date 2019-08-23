89f0fc3a91aa0670d6ed2be920a4f99aca03ca7c
humbertojacobm <humbertojacobm@gmail.com>
2019-08-22 22:08:47 -0500

    module: editing data -> adding create method in services

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -11,6 +11,7 @@ namespace OdeToFood.Data
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
         int Commit();
     }
 
@@ -32,6 +33,13 @@ namespace OdeToFood.Data
         {
             return restaurants.SingleOrDefault(r => r.Id == id);
 
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
         }
 
         public Restaurant Update(Restaurant updatedRestaurant)
